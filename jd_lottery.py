ce85227dd9472c95ddf52bc7a146d3152b688add
fy1128 <l.fuying@yahoo.com>
2017-03-12 15:14:31 +0800

    Now support do drawing with keyword watching.

--- jd_lottery.py
+++ jd_lottery.py
@@ -4,7 +4,10 @@
 import requests
 
 import re
-import time
+import time,datetime,msvcrt
+import httplib2,urllib.request,http.cookiejar
+from multiprocessing.dummy import Pool as ThreadPool
+
 import random
 
 import smtplib
@@ -14,7 +17,7 @@ import configparser#read and save configuration.
 import ast
 import json
 
-import sys
+import sys,os
 import webbrowser
 import codecs
 
@@ -28,7 +31,30 @@ def beep():
     Dur = 200 # Set Duration To 1000 ms == 1 second
     winsound.Beep(Freq,Dur)
     """
-
+def readInput(caption, default, timeout=10):
+    start_time = time.time()
+    sys.stdout.write('%s(%d秒自动跳过):' % (caption,timeout))
+    sys.stdout.flush()
+    input = ''
+    while True:
+        ini=msvcrt.kbhit()
+        try:
+            if ini:
+                chr = msvcrt.getche()
+                if ord(chr) == 13:  # enter_key
+                    break
+                elif ord(chr) >= 32:
+                    input += chr.decode()
+        except Exception as e:
+            pass
+        if len(input) == 0 and time.time() - start_time > timeout:
+            break
+    print ('')  # needed to move to next line
+    if len(input) > 0:
+        return input+''
+    else:
+        return default
+ 
 class Settings:
     """
     Read settings from config.ini .
@@ -69,6 +95,35 @@ class Settings:
             print(str(e))
             return ""
 
+    def get_valid_time(self):
+        try:
+            time_str=self.config.get("通用设置", "放水间隔")
+            time_str_list=time_str.replace("x", "*").replace("X", "*").split("*")
+            seconds=1
+            for i in time_str_list:
+                seconds=seconds*int(i)
+            if seconds>0:
+                return seconds
+            else:
+                return 60
+        except Exception as e:
+            print(str(e))
+            return 60
+
+    def get_draw_time(self):
+        try:
+            time_str=self.config.get("通用设置", "抽奖间隔")
+            seconds=1
+            seconds=seconds*int(time_str)
+            if seconds>0:
+                return seconds
+            else:
+                return 10
+        except Exception as e:
+            print(str(e))
+            return 10
+            
+            
     def is_debug(self):
         try:
             debug_str=self.config.get("通用设置", "调试")
@@ -134,6 +189,42 @@ class Settings:
             print(str(e))
             return ""
 
+    def GetFileList(self, dir, fileList):
+        newDir = dir
+        if os.path.isfile(dir):
+            fileList.append(dir)
+        elif os.path.isdir(dir):  
+            for s in os.listdir(dir):
+                if s.endswith('.txt'):
+                    newDir=os.path.join(dir,s)
+                    self.GetFileList(newDir, fileList)  
+        return fileList
+
+    def get_users(self):
+        users = {}
+        uid = 0
+        for e in COOKIES:
+            uid += 1
+            users[uid] = e
+        return users
+
+    def get_user(self):
+        print('')
+        uid = 0
+        for k in users:
+            uid += 1
+            print(str(k)+') '+users[k])
+        u=int(input('\n请选择用户：'))
+        if u in users.keys():
+            user = users[u].replace('\\','/').split('/')
+            user = user[1].split('.')
+            user = user[0]
+            print('\n你选择了: '+user+'')
+            return u
+        else:
+            print('\n用户不存在，请重输！')
+            self.get_user()
+            
 settings = Settings()
 s=requests.session()
 
@@ -141,6 +232,8 @@ MAX_PRICE=10000000
 #SLEEP_INTERVAL seconds.
 SLEEP_INTERVAL=settings.get_sleep_interval()
 MONITORING_CODE=settings.get_MONITORING_CODE()
+VALID_TIME=settings.get_valid_time()
+DRAW_TIME=settings.get_draw_time()
 DEBUG=settings.is_debug()
 
 PLAY_MUSIC=settings.is_play_music()
@@ -151,6 +244,8 @@ SENDER_EMAIL_ACCOUNT=settings.get_sender_email_account()
 SENDER_EMAIL_PASSWD=settings.get_sender_email_passwd()
 RECEIVER_EMAIL_ACCOUNTS=settings.get_receiver_email_account()
 
+COOKIES = settings.GetFileList('cookies', [])
+USERS = settings.get_users()
 
 class Product:
     def __init__(self,code_i):
@@ -163,13 +258,15 @@ class Product:
     def load_html(self):
         try:
             headers={'Host':'ls-activity.jd.com',
-            'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
+            'User-Agent':'Mozilla/5.0(WindowsNT6.3;WOW64;rv:47.0)Gecko/20100101Firefox/47.0',
             'Accept':'*/*',
             'Accept-Encoding':'gzip, deflate, sdch',
             'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
-            'Referer':'http://m.jd.com/',
+            'Referer':'http://www.jd.com/',
             'Connection':'keep-alive'}
             req=s.get(self.url,headers=headers)
+            if req.status_code != requests.codes.ok:
+                raise Exception(req.status_code)
             self.page=req.text
         except Exception as e:
             print(str(e))
@@ -179,7 +276,6 @@ class Product:
             time.sleep(1)
             self.__init__(self.code_i)
             self.load_html()
-            return None
         self.html=self.page
         return self.html
 
@@ -261,17 +357,117 @@ def send_mail(sub,content,to_list=RECEIVER_EMAIL_ACCOUNTS):
             print("网络故障，稍后重试...")
             return False
 
+def get_webservertime(wintime):
+    while True:
+        try:
+            conn=httplib2.Http(timeout=30)
+            resp, content=conn.request("http://ls-activity.jd.com", "GET")
+            #r.getheaders() #获取所有的http头
+            ts=resp['date'] #获取http头date部分
+            #将GMT时间转换成北京时间
+            ltime= time.strptime(ts[5:25], "%d %b %Y %H:%M:%S")
+            nowtime=time.localtime(time.mktime(ltime)+8*60*60)
+            date=str(nowtime[0])+'-'+str(nowtime[1])+'-'+str(nowtime[2])
+            print('\n时间校对: '+str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))+' / '+str(nowtime[0])+'-'+str(nowtime[1])+'-'+str(nowtime[2])+' '+str(nowtime[3])+':'+str(nowtime[4])+':'+str(nowtime[5]))
+            print('上次放水时间：'+str(wintime))
+            wintime=time.strptime(str(wintime),'%Y-%m-%d %H:%M:%S')
+            wintime=datetime.datetime(wintime[0],wintime[1],wintime[2],wintime[3],wintime[4],wintime[5])
+            nowtime=datetime.datetime(nowtime[0],nowtime[1],nowtime[2],nowtime[3],nowtime[4],nowtime[5])
+            return {'date': date, 'delaytime': (nowtime-wintime).seconds}
+        except Exception as e:
+            print(str(e))
+            beep()
+            print(time.ctime())
+            print("无法获取服务器时间。1秒后重试...")
+            time.sleep(1)
+        
+
+def get_page(user):
+    cj=http.cookiejar.MozillaCookieJar()
+    cj.load(user,ignore_expires=True,ignore_discard=True)
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    urllib.request.install_opener(opener)
+    headers={'Host':'l-activity.jd.com',
+    'User-Agent':'Mozilla/5.0(WindowsNT6.3;WOW64;rv:47.0)Gecko/20100101Firefox/47.0',
+    'Accept':'application/json,text/javascript,*/*;q=0.01',
+    'Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'X-Requested-With':'XMLHttpRequest',
+    'Referer':'http://www.jd.com',
+    'Connection':'keep-alive'}
+    #opener.addheaders = [('Host', 'l-activity.jd.com'), ('User-Agent', 'Mozilla/5.0(WindowsNT6.3;WOW64;rv:47.0)Gecko/20100101Firefox/47.0'), ('Accept', '*/*'), ('X-Requested-With', 'XMLHttpRequest'), ('Referer', 'http://www.jd.com'), ('Connection', 'keep-alive')]
+    user = user.replace('\\','/').split('/')
+    user = user[1].split('.')
+    user = user[0]
+    DRAW_URL="http://l-activity.jd.com/lottery/lottery_start.action?callback=&lotteryCode=%s&_=%d"%(CODE_PENDING, int(time.time()*1000))
+    print("抽奖链接："+DRAW_URL+"\n")
+    if CODE_PENDING in OUT.keys() and 'user' in OUT[CODE_PENDING].keys() and user == OUT[CODE_PENDING]['user'] and time.strptime(OUT[CODE_PENDING]['date'], "%Y-%m-%d")[2] == time.strptime(SERVERDATE, "%Y-%m-%d")[2]:
+        print('当天抽奖次数已用完...\n')
+        return False
+    
+    req=urllib.request.Request(DRAW_URL,headers=headers)
+
+    for i in range(0,3,1):
+        #response=urllib.request.urlopen(req,timeout=6)
+        #response=urllib.request.urlopen(DRAW_URL, timeout=6)
+        try:
+            response=opener.open(req, timeout=6)
+            page=response.read().decode('utf-8', 'ignore')
+            print(datetime.datetime.now().strftime('%H:%M:%S')+' / '+user+': '+str(page))
+            page=json.loads(page)
+            if "data" in page.keys():
+                page = page['data']
+                if page['chances'] == 0:
+                    OUT[CODE_PENDING] = {}
+                    OUT[CODE_PENDING]['user'] = user
+                    OUT[CODE_PENDING]['date'] = SERVERDATE
+                DREW = True
+            page['userPin'] = page['userPin'] if 'userPin' in page and page['userPin'] else user
+            page['drawTime'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            page = json.dumps(page)
+            with codecs.open('src/draw.js', 'a', 'utf-8') as draw:
+                draw.write('draw["'+CODE_PENDING+'"].push('+page+');\n')
+        except Exception as e:
+            print(str(e))
+        time.sleep(DRAW_TIME)
+        continue
+        if url_params.hostname == 'a.jd.com':
+            print(datetime.datetime.now().strftime('%H:%M:%S')+' / '+user+': '+response.read())
+        elif url_params.hostname == 'coupon.jd.com':
+            cer=re.compile('<h1 class="ctxt02"><s class="icon-redbag"></s>(.*)</h1>',flags=0)
+            strlist=cer.findall(response.read().decode('utf-8', 'ignore'))
+            if not strlist:
+                print(datetime.datetime.now().strftime('%H:%M:%S')+' / '+user+': '+'未知错误')
+            else:
+                print(datetime.datetime.now().strftime('%H:%M:%S')+' / '+user+': '+strlist[0])
+
 def Run():
 
+    global OUT, DREW
+    #dict 存储当天已用完抽奖次数的结果
+    OUT = {}
+    DREW = False
     #Counting querying times.
     count=0
     #JD goods id.
     CODE=re.findall(r"(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})", MONITORING_CODE)
     CODE=list(set(CODE))
     while(True):
+        clean=readInput('\n是否清理抽奖记录（Y/N）：', 'N').upper()
+        if clean in ['Y', 'N']:
+            clean='w' if clean == 'Y' else 'a'
+            break
+        else:
+            print('输入不正确！清理（Y）/保留（N）：')
+            
+    with codecs.open('src/draw.js', clean, 'utf-8') as draw:
+        draw.write('var draw={};\n')
+        for c in CODE:
+            draw.write('draw["'+c+'"] = [];\n')
+            
+    while(True):
         #Format output
-        with codecs.open('output.html', 'w', 'utf-8') as html:
-            html.write(u'<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>\n        <meta name="format-detection" content="telephone=no" />\n        <meta http-equiv="refresh" content="10">\n        <title>京东抽奖测水</title>\n        <link href="src/bootstrap.min.css" rel="stylesheet" type="text/css" />\n        <script type="text/javascript" src="src/jquery-3.1.0.min.js"></script>\n        <script type="text/javascript" src="src/data.js"></script>\n        <script type="text/javascript" src="src/angular.min.js"></script>\n        <script type="text/javascript" src="src/init.js"></script>\n    </head>\n    <body>\n        <div ng-app="data" class="container">\n            <div ng-controller="list" class="table-responsive">\n                <div ng-repeat="(code, res) in result">\n                <table class="table table-striped">\n                    <caption>用户列表:  {{code}}</caption>\n                    <tr>\n                        <th>优惠券名称</th>\n                        <th>用户</th>\n                        <th>时间</th>\n                    </tr>\n                    <tr ng-repeat="item in res track by $index">\n                        <td>{{item.prizeName}}</td>\n                        <td>{{item.userPin}}</td>\n                        <td>{{item.winDate}}</td>\n                     </tr>\n                </table>\n                </div>\n            </div>\n        </div>')
+        #with codecs.open('output.html', 'w', 'utf-8') as html:
+        #    html.write(u'<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>\n        <meta name="format-detection" content="telephone=no" />\n        <meta http-equiv="refresh" content="10">\n        <title>京东抽奖测水</title>\n        <link href="src/bootstrap.min.css" rel="stylesheet" type="text/css" />\n        <script type="text/javascript" src="src/jquery-3.1.0.min.js"></script>\n        <script type="text/javascript" src="src/data.js"></script>\n        <script type="text/javascript" src="src/angular.min.js"></script>\n        <script type="text/javascript" src="src/init.js"></script>\n    </head>\n    <body>\n        <div ng-app="data" class="container">\n            <div ng-controller="list" class="table-responsive">\n                <div ng-repeat="(code, res) in result">\n                <table class="table table-striped">\n                    <caption>用户列表:  {{code}}</caption>\n                    <tr>\n                        <th>优惠券名称</th>\n                        <th>用户</th>\n                        <th>时间</th>\n                    </tr>\n                    <tr ng-repeat="item in res track by $index">\n                        <td>{{item.prizeName}}</td>\n                        <td>{{item.userPin}}</td>\n                        <td>{{item.winDate}}</td>\n                     </tr>\n                </table>\n                </div>\n            </div>\n        </div>')
         with codecs.open('src/data.js', 'w', 'utf-8') as data:
             data.write('var data = {};')
         #Endless querying with sleep interval, until user quit the program.
@@ -293,6 +489,7 @@ def Run():
             prev_url=result.get_url(code_i)
             prev_data=result.get_data(code_i)
             news=False
+            match=None
 
             curr_data=product.load_html()
 
@@ -305,6 +502,24 @@ def Run():
             if curr_data!=prev_data:
                 news=True
                 result.set_data(code_i, curr_data)
+                keyword=re.search(code_i+"\|"+".*", MONITORING_CODE)
+                if keyword:
+                    keyword=keyword.group(0).split("|")[1]
+                    match=re.search("{\"prizeName\"\s*:\s*\"[^\"]*"+keyword+"[^\"]*\"[^}]*}", curr_data)
+                    if match:
+                        match=json.loads(match.group())
+                        delaytime=get_webservertime(match['winDate'])
+                        global SERVERDATE
+                        SERVERDATE = delaytime['date']
+                        delaytime = delaytime['delaytime']
+                        if delaytime <= int(VALID_TIME):
+                            global CODE_PENDING
+                            CODE_PENDING = code_i
+                            pool = ThreadPool(len(USERS))
+                            results = pool.map(get_page, COOKIES)
+                            pool.close()
+                            pool.join()
+                        
 
             #detection finished.
             ########################################################
@@ -312,8 +527,8 @@ def Run():
             message = u"数据      ："+curr_data+"\n"+          \
                       u"网址      ："+url_i+"\n"
 
-            if news:
-                print("检测到变更！！！")
+            if DREW:
+                print("\n检测到变更！！！")
                 if SEND_EMAIL:
                     #Try 10 times if unabled to send email.
                     for i in range(10):
@@ -329,7 +544,7 @@ def Run():
                     webbrowser.open(MUSIC_PATH)
 
             else:
-                print("无变更。")
+                print("无变更。\n")
                 
             with codecs.open('src/data.js', 'a', 'utf-8') as data:
                 data.write('\ndata["' + code_i + '"] = ' + curr_data + ';')
@@ -346,8 +561,8 @@ def Run():
             print(change_print)
             
         #Format output
-        with codecs.open('output.html', 'a', 'utf-8') as html:
-            html.write(u'\n    </body>\n</html>')
+        #with codecs.open('output.html', 'a', 'utf-8') as html:
+        #    html.write(u'\n    </body>\n</html>')
             
         #Randomness added to sleep interval, simulating human clicking and refreshing.
         RAND_INT = random.randint(-5,5)
@@ -374,28 +589,6 @@ if __name__ == '__main__':
         try:
             Run()
         except Exception as e:
-            print(e)
+            print(str(e))
             beep()
             time.sleep(5)
-
-
-'''
-#Price: another way to obtain
-import requests
-pids=1217524
-url="http://p.3.cn/prices/mgets?skuIds=J_1217524"# + str(pids)
-
-ret=requests.get(url)
-print(ret)
-print(ret.content)
-'''
-
-
-r'''
-HACK:
-ConfigParser, SafeConfigParser and RawConfigParser react with the no escaped "%" differently:
-invalid interpolation syntax in '【北京市家电节能补贴 最高补贴13%！】' at position 17
-Traceback (most recent call last):
-ValueError: invalid interpolation syntax in '【北京市家电节能补贴 最高补贴13%！】' at position 17
-
-'''
--- src/init.js
+++ src/init.js
@@ -1,10 +1,23 @@
-console.log(data);
-result = {};
-$.each(data, function(k, v) {
-    result[k] = v['data'];
-});
-var app = angular.module("data", []);
-app.controller("list", function ($scope, $http) {
-    $scope.result = result;
-    //$scope.items = data["e70e381a-29a9-4361-ba47-bce3b2e72348"]["data"];
-});
+if (typeof(data) != "undefined") {
+    console.log(data);
+    result = {};
+    $.each(data, function(k, v) {
+        result[k] = v['data'];
+    });
+    var app = angular.module("data", []);
+    app.controller("list", function ($scope, $http) {
+        $scope.result = result;
+        //$scope.items = data["e70e381a-29a9-4361-ba47-bce3b2e72348"]["data"];
+    });
+} else if (typeof(draw) != "undefined") {
+    var app = angular.module("draw", []);
+    app.filter('reverse', function() {
+        return function(items) {
+            return items.slice().reverse();
+      };
+    });
+    app.controller("list", function ($scope, $http) {
+        $scope.result = draw;
+        //$scope.items = data["e70e381a-29a9-4361-ba47-bce3b2e72348"]["data"];
+    });
+}
