2ef9158f672a0f5275bb8bd95ae6e143fddf1985
fy1128 <892973009@qq.com>
2017-03-21 20:31:37 +0800

    BUG fixed: when no charset detect from web source, wrong returned.

--- gencodes.py
+++ gencodes.py
@@ -6,7 +6,7 @@ import json
 import os,sys
 import codecs,random
 import time, datetime
-import httplib2,urllib.request
+import urllib.request
 from bs4 import BeautifulSoup
 
 try:
@@ -17,7 +17,10 @@ except ImportError:
 class MyHTTPRedirectHandler(urllib.request.HTTPRedirectHandler):
     def http_error_302(self, req, fp, code, msg, headers):
         #print(cookieprocessor.cookiejar)
-        return urllib.request.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+        result = urllib2.HTTPError(req.get_full_url(), code, msg, headers, fp)
+        result.status = code
+        return result
+        #return urllib.request.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
 
     http_error_301 = http_error_303 = http_error_307 = http_error_302
 
@@ -42,7 +45,8 @@ def extract(text, sub1, sub2):
     extract a substring from text between first
     occurances of substrings sub1 and sub2
     """
-    return text.split(sub1, 1)[-1].split(sub2, 1)[0]
+    charset = text.split(sub1, 1)[-1].split(sub2, 1)[0]
+    return charset if charset in ['utf-8', 'utf8', 'gbk', 'gb2312', 'gb18030'] else None
     
 def load_html(url):
     while True:
@@ -70,62 +74,71 @@ script_name = argv0_list[len(argv0_list) - 1]
 script_name = script_name[0:-3]
 tmp_record = script_name+'@'+str(os.getpid())+'.txt'
 
-for url in urls:
-    content = load_html(url)
-    encoding = extract(str(content).lower(), 'charset=', '"')
-    #print('-'*50)
-    #print( "Encoding type = %s" % encoding )
-    #print('-'*50)
-    if encoding:
-    # note that Python3 does not read the html code as string
-    # but as html code bytearray, convert to string with
-        content = content.decode(encoding, 'ignore').replace(u'\xa9', u'')
-    else:
-        print("Encoding type not found!")
-    soup = BeautifulSoup(content, 'lxml')
-    title = ' '.join(' '.join(soup.title.string.split('-')[0:-1]).strip().split())
-    match = re.search("\w{8,}-\w{4,}-\w{4,}-\w{4,}-\w{12,}", str(content))
-    if match is not None:
-        code = match.group(0)
-        #url_w = "http://ls-activity.jd.com/lotteryApi/getWinnerList.action?lotteryCode="+code+"&_=%d&callback="%(int(time.time()*1000))
-        #winner = load_html(url_w)
-        #winner = json.loads(str(load_html(url_w), 'utf-8'))
-        #if 'data' in winner.keys() and len(winner['data']) > 0:
-        #    winner = winner['data']
-        #    awards = list(set([i['prizeName'] for i in winner]))
-        #    awards = ', '.join(random.sample(awards, random.randint(1, len(awards) if len(awards) < 3 else 3)))
-        #    desc = awards+' - '+title
-        #    with codecs.open(tmp_record, 'a+', 'utf-8') as record:
-        #        record.write(code+'\n; '+url+' '+desc+'\n')
-        #else:
-        #    awards = '没有中奖记录'
-        
-        url_i = "http://ls-activity.jd.com/lotteryApi/getLotteryInfo.action?callback=&lotteryCode=%s&_=%d"%(code, int(time.time()*1000))
-        prize = json.loads(str(load_html(url_i), 'utf-8'))
-        if 'data' in prize.keys() and len(prize['data']) > 0:
-            prize = prize['data']
-            etime = time.strptime(str(prize['endTime']),'%Y-%m-%d %H:%M:%S')
-            etime=datetime.datetime(etime[0],etime[1],etime[2],etime[3],etime[4],etime[5])
-            awards = list(set([i['prizeName'] for i in prize['lotteryPrize']]))
-            awards = '，'.join(awards)
-            desc = awards+' - '+prize['lotteryName']
-            if (datetime.datetime.now() - etime).days <= 0:
-                vtime = prize['beginTime']+' - '+prize['endTime']
-                with codecs.open(tmp_record, 'a+', 'utf-8') as record:
-                    record.write(code+'\n; '+url+' '+desc+'\n')
-            else:
-                vtime = "已过期"
-        print('-'*50)
-        print('抽奖代码：'+code)
-        print('奖品举例：'+awards)
-        print('活动时间：'+vtime)
+try:
+    for url in urls:
+        content = load_html(url)
+        encoding = extract(str(content).lower(), 'charset=', '"')
+        #print('-'*50)
+        #print( "Encoding type = %s" % encoding )
+        #print('-'*50)
+        if encoding:
+        # note that Python3 does not read the html code as string
+        # but as html code bytearray, convert to string with
+            content = content.decode(encoding, 'ignore').replace(u'\xa9', u'')
+        else:
+            print("Debug: Encoding type not found!")
+        match = re.search("\w{8,}-\w{4,}-\w{4,}-\w{4,}-\w{12,}", str(content))
+        if match is not None:
+            soup = BeautifulSoup(content, 'lxml')
+            title = ' '.join(' '.join(soup.title.string.split('-')[0:-1]).strip().split())
+            code = match.group(0)
+            #url_w = "http://ls-activity.jd.com/lotteryApi/getWinnerList.action?lotteryCode="+code+"&_=%d&callback="%(int(time.time()*1000))
+            #winner = load_html(url_w)
+            #winner = json.loads(str(load_html(url_w), 'utf-8'))
+            #if 'data' in winner.keys() and len(winner['data']) > 0:
+            #    winner = winner['data']
+            #    awards = list(set([i['prizeName'] for i in winner]))
+            #    awards = ', '.join(random.sample(awards, random.randint(1, len(awards) if len(awards) < 3 else 3)))
+            #    desc = awards+' - '+title
+            #    with codecs.open(tmp_record, 'a+', 'utf-8') as record:
+            #        record.write(code+'\n; '+url+' '+desc+'\n')
+            #else:
+            #    awards = '没有中奖记录'
             
-seen = set()
-with codecs.open('record.txt', 'w', 'utf-8') as record:
-    with codecs.open(tmp_record, 'r', 'utf-8') as trecord:
-        for line in trecord:
-            if line not in seen and not line.startswith(';'):
-                record.write(line+next(trecord))
-                seen.add(line)
-
-os.remove(tmp_record)
+            url_i = "http://ls-activity.jd.com/lotteryApi/getLotteryInfo.action?callback=&lotteryCode=%s&_=%d"%(code, int(time.time()*1000))
+            prize = json.loads(str(load_html(url_i), 'utf-8'))
+            if 'data' in prize.keys() and len(prize['data']) > 0:
+                prize = prize['data']
+                etime = time.strptime(str(prize['endTime']),'%Y-%m-%d %H:%M:%S')
+                etime=datetime.datetime(etime[0],etime[1],etime[2],etime[3],etime[4],etime[5])
+                awards = list(set([i['prizeName'] for i in prize['lotteryPrize']]))
+                awards = '，'.join(awards)
+                desc = awards+' - '+prize['lotteryName']
+                if (datetime.datetime.now() - etime).days <= 0:
+                    vtime = prize['beginTime']+' - '+prize['endTime']
+                    with codecs.open(tmp_record, 'a+', 'utf-8') as record:
+                        record.write(code+'\n; '+url+' '+desc+'\n')
+                else:
+                    vtime = "已过期"
+            print('-'*50)
+            print('抽奖代码：'+code)
+            print('奖品举例：'+awards)
+            print('活动时间：'+vtime)
+            print('链接地址：'+url)
+        else:
+            print('-'*50)
+            print('没有找到抽奖代码，请检查链接。')
+            print('链接地址：'+url)
+                
+    seen = set()
+    with codecs.open('record.txt', 'w', 'utf-8') as record:
+        with codecs.open(tmp_record, 'r', 'utf-8') as trecord:
+            for line in trecord:
+                if line not in seen and not line.startswith(';'):
+                    record.write(line+next(trecord))
+                    seen.add(line)
+except Exception as e:
+    print(e)
+finally:
+    if os.path.isfile(tmp_record):
+        os.remove(tmp_record)
